608ebc96f10870332f46b069fd5b753a8aaea24b
Irina Klepova <ivklepova23@gmail.com>
2024-10-07 13:57:24 +0400

    внесены изменения в оформление

--- users/permissions.py
+++ users/permissions.py
@@ -2,13 +2,13 @@ from rest_framework.permissions import BasePermission
 
 
 class IsStaff(BasePermission):
-
+    """Проверяет, что пользователь является модератором"""
     def has_permission(self, request, view):
         return request.user.groups.filter(name="moderator").exists()
 
 
 class IsOwner(BasePermission):
-
+    """Проверяет, что пользователь является владельцем"""
     def has_object_permission(self, request, view, obj):
         if obj.user == request.user:
             return True
