8f14aa34292bc0547eddd84ce02977784fa3b0d0
Irina Klepova <ivklepova23@gmail.com>
2024-10-07 12:12:40 +0400

    внесены исправления в оформление

--- trackers/serializers.py
+++ trackers/serializers.py
@@ -81,36 +81,27 @@ class ImportantTasksSerializer(serializers.ModelSerializer):
     @staticmethod
     def get_employees(self):
         important_tasks = Task.objects.filter(
-            Q(is_active=False)
-            & (
-                Q(parent_task__is_active=True)
-                | Q(parent_task__parent_task__is_active=True)
-                | Q(parent_task__parent_task__parent_task__is_active=True)
-            )
+            Q(is_active=False) & (Q(parent_task__is_active=True) |
+                                  Q(parent_task__parent_task__is_active=True) |
+                                  Q(parent_task__parent_task__parent_task__is_active=True)
+                                  )
         )
         imp_task_id = []
         for task in important_tasks:
             imp_task_id.append(task.id)
 
         employee_min_task = (
-            Employee.objects.all()
-            .annotate(task_count=Count("tasks"))
-            .order_by("task_count")
-            .first()
+            Employee.objects.all().annotate(task_count=Count("tasks")).order_by("task_count").first()
         )
         emt_id = employee_min_task.id
         count_employee_min_task = Task.objects.filter(executor__id=emt_id).count()
         available_employees = []
         for i in imp_task_id:
             task_parent = Task.objects.get(pk=i).parent_task
-            employee_task_parent = Employee.objects.filter(
-                tasks__id=task_parent.id
-            ).first()
+            employee_task_parent = Employee.objects.filter(tasks__id=task_parent.id).first()
             if employee_task_parent is not None:
                 etp_id = employee_task_parent.id
-                count_employee_task_parent = Task.objects.filter(
-                    executor__id=etp_id
-                ).count()
+                count_employee_task_parent = Task.objects.filter(executor__id=etp_id).count()
 
                 if count_employee_task_parent - count_employee_min_task <= 2:
                     emp = Employee.objects.filter(pk=etp_id)
