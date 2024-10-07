7b6a9e13c899bb094d590755d38905161ddece36
Irina Klepova <ivklepova23@gmail.com>
2024-10-07 12:14:49 +0400

    внесены исправления в оформление

--- trackers/urls.py
+++ trackers/urls.py
@@ -18,27 +18,16 @@ router = DefaultRouter()
 router.register(r"employees", EmployeeViewSet, basename="employees")
 
 urlpatterns = [
-    path(
-        "task/task_list/",
-        TaskListAPIView.as_view(),
-        name="task_list",
-    ),
-    path(
-        "task/retrieve/<int:pk>/",
-        TaskRetrieveAPIView.as_view(),
-        name="task_retrieve",
-    ),
-    path("task/create/", TaskCreateAPIView.as_view(), name="task_create"),
-    path("task/update/<int:pk>/", TaskUpdateAPIView.as_view(), name="task_update"),
-    path("task/delete/<int:pk>/", TaskDeleteAPIView.as_view(), name="task_delete"),
-    path(
-        "employee_track/employee_track_list/",
-        EmployeeTrackAPIView.as_view(),
-        name="employee_track_list",
-    ),
-    path(
-        "important_tasks/important_tasks_list/",
-        ImportantTasksForEmployeesAPIView.as_view(),
-        name="important_tasks_list",
-    ),
-] + router.urls
+                  path("task/task_list/", TaskListAPIView.as_view(), name="task_list"),
+                  path("task/retrieve/<int:pk>/", TaskRetrieveAPIView.as_view(), name="task_retrieve"),
+                  path("task/create/", TaskCreateAPIView.as_view(), name="task_create"),
+                  path("task/update/<int:pk>/", TaskUpdateAPIView.as_view(), name="task_update"),
+                  path("task/delete/<int:pk>/", TaskDeleteAPIView.as_view(), name="task_delete"),
+                  path("employee_track/employee_track_list/", EmployeeTrackAPIView.as_view(),
+                       name="employee_track_list"),
+                  path(
+                      "important_tasks/important_tasks_list/",
+                      ImportantTasksForEmployeesAPIView.as_view(),
+                      name="important_tasks_list"
+                  ),
+              ] + router.urls
