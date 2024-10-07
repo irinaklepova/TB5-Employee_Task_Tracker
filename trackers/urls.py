a4aed427ac07d0824dc957eca03a2d53504fc230
Irina Klepova <ivklepova23@gmail.com>
2024-10-07 13:56:32 +0400

    внесены изменения в оформление

--- trackers/urls.py
+++ trackers/urls.py
@@ -18,24 +18,12 @@ router = DefaultRouter()
 router.register(r"employees", EmployeeViewSet, basename="employees")
 
 urlpatterns = [
-    path(
-        "task/task_list/",
-        TaskListAPIView.as_view(),
-        name="task_list",
-    ),
-    path(
-        "task/retrieve/<int:pk>/",
-        TaskRetrieveAPIView.as_view(),
-        name="task_retrieve",
-    ),
+    path("task/task_list/", TaskListAPIView.as_view(), name="task_list"),
+    path("task/retrieve/<int:pk>/", TaskRetrieveAPIView.as_view(), name="task_retrieve"),
     path("task/create/", TaskCreateAPIView.as_view(), name="task_create"),
     path("task/update/<int:pk>/", TaskUpdateAPIView.as_view(), name="task_update"),
     path("task/delete/<int:pk>/", TaskDeleteAPIView.as_view(), name="task_delete"),
-    path(
-        "employee_track/employee_track_list/",
-        EmployeeTrackAPIView.as_view(),
-        name="employee_track_list",
-    ),
+    path("employee_track/employee_track_list/", EmployeeTrackAPIView.as_view(), name="employee_track_list"),
     path(
         "important_tasks/important_tasks_list/",
         ImportantTasksForEmployeesAPIView.as_view(),
